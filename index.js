92561e4ba59eef7d138b36849998fda0c7413882
Celia Marshall <celia.m@gmail.com>
2018-10-01 15:01:12 -0700

    buttons will turn ON color only

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 (function() {
   'use strict';
+  const stopButton = document.querySelector('#stopButton')
+  function turnRed() {
+    const stopLight = document.querySelector('#stopLight')
+    stopLight.classList.add('stop')
+  }
+  stopButton.addEventListener ('click', turnRed)
+  
+  const slowButton = document.querySelector('#slowButton')
+  function turnYellow() {
+    document.querySelector('#slowLight').classList.add('slow')
+  }
+  slowButton.addEventListener('click', turnYellow)
 
-  // YOUR CODE HERE
+  const goButton = document.querySelector('#goButton')
+  function turnGreen() {
+    document.querySelector('#goLight').classList.add('go')
+  }
+  goButton.addEventListener('click', turnGreen)
 })();
